47216bfa1fe24bfa199fff38bb0507241e5b020a
QuickTIger <wjzhh815@gmail.com>
2013-04-29 12:29:19 -0400

    a not bad version

--- html_get.cpp
+++ html_get.cpp
@@ -18,7 +18,7 @@ using namespace std;
 
 
 #define PORT 80
-#define MAX_BUF 2*1024*1024
+#define MAX_BUF 2*1024*1024*2
 
 int urlparser(char * url,  char * url_host, char * url_link){
 
@@ -85,7 +85,7 @@ int gethtml(char * buf, char * url_host, char * url_link) {
         exit(1);
     }
         /*Set Http Haeder*/
-    char request[256];
+    char request[512];
     sprintf(request,"GET %s HTTP/1.1 \r\nHost: %s \r\nConnection: keep-alive\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: deflate\r\nAccept-Language: en-US,en;q=0.8\r\nConnection: close\r\n\r\n",url_link,url_host);
    //printf("%s\n",request);
         /*Send Data*/
@@ -112,7 +112,11 @@ void insert_url(char * url , vector<string> & urls, vector<string> & urls_curren
 
 	if (find(urls.begin(), urls.end(), url)==urls.end()){
         urls.push_back(url);
-                if ((string(url).find(".gif",0)==string::npos)&&(string(url).find(".jpg",0)==string::npos)&&(string(url).find(".js",0)==string::npos)){
+                if ((string(url).find(".gif",0)==string::npos)\
+                &&(string(url).find(".jpg",0)==string::npos)\
+                &&(string(url).find(".swf",0)==string::npos)\
+                &&(string(url).find(".doc",0)==string::npos)\
+                &&(string(url).find(".rar",0)==string::npos)){
             urls_current.push_back(url);
 		}
 	}
@@ -136,9 +140,32 @@ int html_parser( char * buf, char * host, vector<string> & urls, vector<string> 
     if(uurl[0]=='/'){
         char tmp_uurl[256];
     	sprintf(tmp_uurl,"http://%s%s",host,uurl);
-	insert_url(tmp_uurl,urls,urls_current);
+        insert_url(tmp_uurl,urls,urls_current);
     	}
-     else if (uurl[0]=='H') {
+    else if (uurl[0]=='h') {
+        insert_url(uurl,urls,urls_current);
+        printf("%s",uurl);
+    	}
+    href_cursor=href_end+1;
+    }
+
+    while(1){
+    char * href_begin=strstr(href_cursor,"href=\'");
+    if (NULL==href_begin){
+    		break;
+    	}
+    char * href_end=strstr(href_begin+6,"\'");
+    if ((href_end-href_begin)>256){
+        href_cursor=href_end+1;
+    	continue;//It is too long to handle.
+    }
+    sprintf(uurl,"%.*s\n",(href_end-href_begin-6),href_begin+6);
+    if(uurl[0]=='/'){
+        char tmp_uurl[256];
+    	sprintf(tmp_uurl,"http://%s%s",host,uurl);
+        insert_url(tmp_uurl,urls,urls_current);
+    	}
+     else if (uurl[0]=='h') {
 	insert_url(uurl,urls,urls_current);
    // 	printf("%s",uurl);
     	}
@@ -146,6 +173,7 @@ int html_parser( char * buf, char * host, vector<string> & urls, vector<string> 
     }
 
     char * src_cursor=h_buf;
+
     while(1){
     char * src_begin=strstr(src_cursor,"src=\"");
     if (NULL==src_begin){
@@ -158,11 +186,34 @@ int html_parser( char * buf, char * host, vector<string> & urls, vector<string> 
     }
     sprintf(uurl,"%.*s\n",(src_end-src_begin-5),src_begin+5);
     if(uurl[0]=='/'){
-       char tmp_uurl[256];
+        char tmp_uurl[256];
     	sprintf(tmp_uurl,"http://%s%s",host,uurl);
-	insert_url(tmp_uurl,urls,urls_current);
+        insert_url(tmp_uurl,urls,urls_current);
     	}
-    else if (uurl[0]=='H'){
+    else if (uurl[0]=='h'){
+	insert_url(uurl,urls,urls_current);
+    	}
+    src_cursor=src_end+1;
+    }
+
+
+    while(1){
+    char * src_begin=strstr(src_cursor,"src=\'");
+    if (NULL==src_begin){
+    		break;
+    	}
+    char * src_end=strstr(src_begin+5,"\'");
+    if ((src_end-src_begin)>256){
+        src_cursor=src_end+1;
+    	continue;//It is too long to handle.
+    }
+    sprintf(uurl,"%.*s\n",(src_end-src_begin-5),src_begin+5);
+    if(uurl[0]=='/'){
+        char tmp_uurl[256];
+    	sprintf(tmp_uurl,"http://%s%s",host,uurl);
+        insert_url(tmp_uurl,urls,urls_current);
+    	}
+    else if (uurl[0]=='h'){
 	insert_url(uurl,urls,urls_current);
     	}
     src_cursor=src_end+1;
@@ -180,30 +231,22 @@ int main() {
 	char url[256];//Url buffer(256 is a reasonable size);
 	char url_host[64];
 	char url_link[256];
-    cout<<"-5"<<endl;
-	strcpy(url,"http://www.njupt.edu.cn/");
+	strcpy(url,"http://youth.njupt.edu.cn/");
 
 	insert_url(url,urls,urls_current);
-	cout<<"-3"<<endl;
 	while(urls_current.begin()!=urls_current.end()){
-		//printf("%s\n%s\n%s\n",url,url_host,url_link);
-		cout<<url<<"\n";
 
+		cout<<url<<"\n";
 		urlparser(url,url_host,url_link);
-		cout<<"-1.5"<<"\n";
-		//printf("html_buf: %s\n",html_buf);
-		//printf("url_host: %s\n",url_host);
-		//printf("url_link: %s\n",url_link);
 
         if (0 == gethtml( html_buf, url_host, url_link )) {
-		//printf("%s",html_buf);
+
 			html_parser(html_buf,url_host, urls, urls_current);
 		}
-		cout<<urls_current.size()<<"========"<<urls.size()<<"\n";
+		cout<<"Urls left in stack :"<<urls_current.size()<<"\t"<<"All find Urls: "<<urls.size()<<"\n";
 
 		strcpy(url,urls_current.back().c_str());
 		urls_current.pop_back();
-		cout<<"3"<<"\n";
 		html_buf[0]='\0';
     	}
    return 0;
